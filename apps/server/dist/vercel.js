18f130fd0fe0e98549bcbd633112b97b14ed5179
Your Name <you@example.com>
2026-01-27 23:24:09 +0800

    fix: revert express import to * as express

--- apps/server/dist/vercel.js
+++ apps/server/dist/vercel.js
@@ -4,8 +4,8 @@ exports.default = handler;
 const core_1 = require("@nestjs/core");
 const platform_express_1 = require("@nestjs/platform-express");
 const app_module_1 = require("./app.module");
-const express_1 = require("express");
-const server = (0, express_1.default)();
+const express = require("express");
+const server = express();
 const createNestServer = async (expressInstance) => {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_express_1.ExpressAdapter(expressInstance));
     app.setGlobalPrefix('api/v1');
