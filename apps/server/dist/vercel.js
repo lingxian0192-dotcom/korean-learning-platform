342ac8b775b3bcbd1501d77f598e20baa88aab79
Your Name <you@example.com>
2026-01-27 22:48:26 +0800

    fix: add @vercel/node dependency

---
+++ apps/server/dist/vercel.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = handler;
+const core_1 = require("@nestjs/core");
+const platform_express_1 = require("@nestjs/platform-express");
+const app_module_1 = require("./app.module");
+const express = require("express");
+const server = express();
+const createNestServer = async (expressInstance) => {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_express_1.ExpressAdapter(expressInstance));
+    app.setGlobalPrefix('api/v1');
+    app.enableCors();
+    await app.init();
+    return app;
+};
+let appPromise;
+async function handler(req, res) {
+    if (!appPromise) {
+        appPromise = createNestServer(server);
+    }
+    await appPromise;
+    server(req, res);
+}
+//# sourceMappingURL=vercel.js.map
---
+++ apps/server/src/vercel.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import * as express from 'express';
+
+const server = express();
+
+const createNestServer = async (expressInstance: express.Express) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  app.setGlobalPrefix('api/v1');
+  app.enableCors();
+  await app.init();
+  return app;
+};
+
+let appPromise: Promise<any>;
+
+export default async function handler(req: any, res: any) {
+  if (!appPromise) {
+    appPromise = createNestServer(server);
+  }
+  await appPromise;
+  server(req, res);
+}
